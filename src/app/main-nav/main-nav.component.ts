7a9c58d46d078ef838d0613b324b59457c2d8955
Felipe Vasconcellos <vasconcellosdefreitas@outlook.com>
2019-03-09 02:08:48 -0300

    implementando navBar

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -3,6 +3,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { NavItem } from '../shared/navItem.model'
+
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
@@ -17,4 +19,37 @@ export class MainNavComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  navItems: NavItem[] = [
+    {
+      displayName: 'Eventos',
+      iconName: '',
+      route: '',
+      children: []
+    },
+    {
+      displayName: 'Cadastro',
+      iconName: '',
+      route: '',
+      children: [
+        {
+          displayName: 'Membro',
+          iconName: '',
+          route: '',
+          children: []
+        },
+        {
+          displayName: 'Aniversário',
+          iconName: '',
+          route: '',
+          children: []
+        },
+        {
+          displayName: 'Consulta',
+          iconName: '',
+          route: '',
+          children: []
+        },
+      ]
+    },
+  ]
 }
